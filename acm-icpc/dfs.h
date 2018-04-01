cc8afedeac5b1573b22fdc879f803010774b74a4
mac <>
2018-04-01 17:29:33 +0800

    deep first search demo

---
+++ acm-icpc/dfs.h
@@ -0,0 +1,12 @@
+//
+// Created by mac on 2018/4/1.
+//
+
+#ifndef ALGORITHMS_DFS_H
+#define ALGORITHMS_DFS_H
+
+
+bool dfs(int i, int sum);
+
+
+#endif //ALGORITHMS_DFS_H
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "acm-icpc/fibonacci.h"
 #include "acm-icpc/stack.h"
 #include "acm-icpc/queue.h"
+#include "acm-icpc/dfs.h"
 
 using namespace std;
 int main() {
@@ -17,6 +18,12 @@ int main() {
     cout << "**************basic queue operation********************" << endl;
     queueOper();
 
+    cout<< "*****************dfs demo**********************"<<endl;
+    if(dfs(0,0)){
+        cout << "Yes!" << endl;
+    } else {
+        cout << "No!" << endl;
+    }
 
     return 0;
 }
