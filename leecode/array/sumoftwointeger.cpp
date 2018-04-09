31ed1200ac4077f6380f702f8fa0cdf0c71fc67a
mac <>
2018-04-09 19:55:45 +0800

    🎉[leecode] sum of two integer without operator + - update

--- leecode/array/sumoftwointeger.cpp
+++ leecode/array/sumoftwointeger.cpp
@@ -5,7 +5,7 @@
 #include "sumoftwointeger.h"
 int getSum(int a, int b) {
     int result = a ^ b; // 按位加
-    int carray = (a & b) << 1; // 计算进位
-    if(carray!=0) return  getSum(result,carray); //判断进位与处理
+    int carry = (a & b) << 1; // 计算进位
+    if(carry!=0) return  getSum(result,carry); //判断进位与处理
     return result;
 }
